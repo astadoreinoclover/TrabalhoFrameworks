dbe7da7ecc821d12ad5e4ed24984dabed37f6f4b
Renato Cardozo <96791439+astadoreinoclover@users.noreply.github.com>
2024-08-29 00:51:10 +0000

    aula-2

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,25 +1,115 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router';
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React from 'react';
+import { View, Text, StyleSheet, Image, TextInput, Button, ScrollView, TouchableHighlight } from 'react-native';
+import { rgbaArrayToRGBAColor } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
+// import myCustomStyles from './_styles';      // Aqui podem importar estilos 
+import ScrollViewIndicator from 'react-native-scroll-indicator';
 
 export default function HomeScreen() {
+
+  const [text, funcao] = React.useState('Useless Text');
+  const [numeroDeCliques, setNumeroDeCliques] = React.useState(0);
+
+  const submitValues = () => {
+    console.log(text);
+  }
+
+  const handleButtonClick = () => {
+    setNumeroDeCliques(numeroDeCliques + 1);
+  }
+
+  const handleTouch = () => {
+    console.log("Clicou na Imagem!!!!");
+  }
+
   return (
-    <>
     <View style={styles.container}>
-      <Text>Home</Text>
-      <Link href="/segundaTela">Ir para segunda tela</Link>
+      <ScrollViewIndicator
+      style={{maxWidth: 750}}
+      shouldIndicatorHide={false}
+      flexibleIndicator= {true}
+      scrollIndicatorStyle = {{backgroundColor: "pink"}}
+      scrollIndicatorContainerStyle= {{backgroundColor: "rgba(0, 0, 0, 0.5)"}}>
+      <Text style={styles.title}>React Native é Show!</Text>
+      <View style={styles.meuBotao}>
+        <Button title={"Este botão foi pressionado " + numeroDeCliques + ' vezes!'} onPress={handleButtonClick}></Button>
+      </View>
+      <Text style= {styles.texto}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+        <Text>Home</Text>
+        <View style={{backgroundColor: 'blue', padding: 50}}>
+          <Text style={{color: 'white'}}>Texto de uma view</Text>
+        </View>
+        
+        <View style={{backgroundColor: 'ccc', padding: 50}}>
+          <TouchableHighlight onPress={handleTouch}>
+            <Image style={styles.logo} source={{uri: 'https://miro.medium.com/v2/resize:fit:1024/1*xDi2csEAWxu95IEkaNdFUQ.png'}}></Image>
+          </TouchableHighlight>
+        </View>
+        <Link href="/segundaTela">Ir para a segunda tela</Link>
+        <View>
+          <TextInput
+            style={styles.input}
+            onChangeText={funcao}
+            value={text}
+          />
+          <Button title='Enviar dados' onPress={submitValues}></Button>
+        </View>
+        <Image
+          style={styles.logo}
+          source={{
+            uri: 'https://reactnative.dev/img/tiny_logo.png',
+          }}
+        />
+      </ScrollViewIndicator>
     </View>
-    </>
   );
 }
 
 const styles = StyleSheet.create({
+  meuBotao: {
+    margin: 20
+  },
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    flexDirection:'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#333",
+    // maxWidth: 750, 
+    // margin: 'auto'
+  },
+  logo: {
+    width: 250,
+    height: 250,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: '#20232a',
+    borderRadius: 6,
+    backgroundColor: '#61dafb',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  texto: {
+    color: "#eee"
   }
-});
+}
+);
--- app/app/segundaTela.tsx
+++ app/app/segundaTela.tsx
@@ -1,12 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router';
 
 export default function SegundaTela() {
   return (
     <>
     <View style={styles.container}>
+    <ScrollView style={{maxWidth: 750}}>
       <Text>Segunda Tela</Text>
       <Link href="/terceiraTela">Ir para Terceira tela</Link>
+      <Text style= {styles.texto}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+    </Text>
+    </ScrollView>
     </View>
     </>
   );
@@ -16,6 +27,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "#333"
+  },
+  texto: {
+    color: "#ccc"
   }
 });
