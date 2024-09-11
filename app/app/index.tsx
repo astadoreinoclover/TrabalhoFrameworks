dea6c6561c8cb4e624a0e52a4cb487ced3224d8c
Renato Cardozo <96791439+astadoreinoclover@users.noreply.github.com>
2024-09-11 23:34:30 +0000

    refactor(all): refactor all project

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,35 +1,18 @@
-import { Link } from 'expo-router';
 import React from 'react';
-import MainText from '@/components/MainText';
-import { View, StyleSheet, Dimensions, PixelRatio } from 'react-native';
-import { rgbaArrayToRGBAColor } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
-// import myCustomStyles from './_styles';      // Aqui podem importar estilos 
-// import ScrollViewIndicator from 'react-native-scroll-indicator';
-import Header from '@/components/Header';
-const { width, height} = Dimensions.get('window');
-const scale = PixelRatio.get();
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './(screens)/HomeScreen';
+import LoginScreen from './(screens)/Login';
 
-export default function HomeScreen() {
+const Stack = createNativeStackNavigator();
 
+export default function App() {
   return (
-    <View style={styles.container}>
-      <Header></Header>
-      <View style={styles.mainText}><MainText></MainText></View>
-      
-    </View>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection:'column',
-    alignItems: 'center',
-    backgroundColor: "#f0f0f0",
-    // height: height * 0.1,
-  },
-  mainText: {
-    color: '#fff'
-  }
-}
-);
