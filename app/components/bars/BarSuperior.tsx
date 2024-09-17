c9715c978a3f6489e4e499e9f81e0ff23077908c
Renato Cardozo <96791439+astadoreinoclover@users.noreply.github.com>
2024-09-17 23:35:56 +0000

    logout

--- app/components/bars/BarSuperior.tsx
+++ app/components/bars/BarSuperior.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, useWindowDimensions, StyleSheet } from 'react-native';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
+import { AuthContext } from '@/contexts/Auth';
 
 const BarSuperior = () => {
     const { width } = useWindowDimensions();
+    const authContext = useContext(AuthContext);
+
+    function logout() {
+        authContext.logout()
+    }
     return (
         <View style={[styles.top, {width: width}]}>
-            <SimpleLineIcons name="logout" size={24} color="red" />
+            <SimpleLineIcons name="logout" size={24} color="red" onPress={logout} />
         </View>
     )
 }
